bc58904258e6c98d8a6591a727224e0cfba3066a
Adel <adelsadeq24@gmail.com>
2021-09-06 18:17:48 +0200

    data pool is done

--- src/main/java/org/library/System/main.java
+++ src/main/java/org/library/System/main.java
@@ -1,11 +1,25 @@
 package org.library.System;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class System {
+class System implements CommandLineRunner{
+    private static final Logger Log = LoggerFactory.getLogger(System.class);
     public static void main(String[] args) {
         SpringApplication.run(System.class, args);
+    }
+    @Value("${application.name")
+    private String applicationName;
+    @Value("${username}")
+    private String username;
+    @Override
+    public void run(String... args) throws Exception {
+        Log.info("Application name {},Username:{}",applicationName,username);
+
     }
 }
