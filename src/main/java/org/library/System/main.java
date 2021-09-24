b9147c013795b192840798dee3d0bff1babb6001
adel@adel-G3-3590 <adelsadeq24@gmail.com>
2021-09-24 15:18:36 +0200

    swagger and deployment done

--- src/main/java/org/library/System/main.java
+++ src/main/java/org/library/System/main.java
@@ -2,8 +2,10 @@ package org.library.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 class System {
     public static void main(String[] args) {
         SpringApplication.run(System.class, args);
