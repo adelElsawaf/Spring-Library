a65625030f9bfb57ba3c5738e7523a5531b0dc98
adel@adel-G3-3590 <adelsadeq24@gmail.com>
2021-09-26 12:00:40 +0200

    create data base is added

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #! /usr/bin/bash
 startDockerPostgres= sudo docker start postgres
 sudoPassword= 1234567
+downloadPostgres= sudo docker pull postgres
+dockerRunDataBaseInstance= sudo docker run -name postgre -e POSTGRES_PASSWORD = mysecretpassword -d -p 5432:5432 postgres
+dataBase= $( sudo docker images -q postgres )
 runSystemlocally= ./gradlew bootRun --args='--spring.profiles.active=local'
-echo $startDockerPostgres"
-echo $sudoPassword"
-echo $runSystemlocally"
+echo "$sudoPassword"
+if [[ -n "$result" ]]; then
+echo "$startDockerPostgres".
+echo "$sudoPassword"
+echo "$runSystemlocally"
+else
+echo "$downloadPostgres"
+echo  "$sudoPassword"
+echo "$dockerRunDataBaseInstance"
+echo "$sudoPassword"
+fi
 
