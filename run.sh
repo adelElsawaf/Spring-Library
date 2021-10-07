0437481c036445fc9d27a14dc37768a8685158ed
adel@adel-G3-3590 <adelsadeq24@gmail.com>
2021-10-07 12:41:08 +0200

    echo is removed

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ startDockerPostgres= sudo docker start postgres
 dataBase= $( sudo docker images -q postgres )
 runSystemlocally= ./gradlew bootRun --args='--spring.profiles.active=local'
 if [[ -name "$dataBase" ]]; then
-echo "$startDockerPostgres".
-echo "$runSystemlocally"
+"$startDockerPostgres".
+"$runSystemlocally"
 else
 downloadPostgres= sudo docker pull postgres
 dockerRunDataBaseInstance= sudo docker run -name postgre -e POSTGRES_PASSWORD = mysecretpassword -d -p 5432:5432 postgres
-echo "$downloadPostgres"
-echo "$dockerRunDataBaseInstance"
+"$downloadPostgres"
+"$dockerRunDataBaseInstance"
 fi
