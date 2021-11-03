eb2cf6dc8480ddb54e3d81ae7e8bea4bea179c97
adel@adel-G3590 <adelsadeq24@gmail.com>
2021-11-03 13:30:50 +0200

    download data base is added

---
+++ data_base.sh
@@ -0,0 +1,1 @@
+sudo docker container run -d --name=postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword postgres
--- run.sh
+++ run.sh
@@ -1,14 +1,2 @@
-startDockerPostgres= sudo docker start postgres
-dataBase= $( sudo docker images -q postgres )
-runSystemlocally= ./gradlew bootRun --args='--spring.profiles.active=local'
-if [[ -name "$dataBase" ]]; then
-"$startDockerPostgres".
-wait
-"$runSystemlocally"
-else
-downloadPostgres= sudo docker pull postgres
-wait
-dockerRunDataBaseInstance= sudo docker run -name postgre -e POSTGRES_PASSWORD = mysecretpassword -d -p 5432:5432 postgres
-"$downloadPostgres"
-"$dockerRunDataBaseInstance"
-fi
+sudo docker start postgres
+./gradlew bootRun --args='--spring.profiles.active=local'
